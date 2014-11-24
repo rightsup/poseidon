25fe1b76d50d894d56dfcdbdb757f93c49751105
szymonsobczak <szymon.sobczak@getbase.com>
2014-11-24 16:35:27 +0100

    remove verbose comments, add link to author

--- lib/poseidon/connection.rb
+++ lib/poseidon/connection.rb
@@ -109,8 +109,8 @@ module Poseidon
       end
     end
 
+    # Explained on http://spin.atomicobject.com/2013/09/30/socket-connection-timeout-ruby/
     def connect_with_timeout(host, port, timeout = 5)
-      # Convert the passed host into structures the non-blocking calls can deal with
       addr = Socket.getaddrinfo(host, nil)
       sockaddr = Socket.pack_sockaddr_in(port, addr[0][3])
 
@@ -118,28 +118,17 @@ module Poseidon
         socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
 
         begin
-          # Initiate the socket connection in the background. If it doesn't fail
-          # immediatelyit will raise an IO::WaitWritable (Errno::EINPROGRESS)
-          # indicating the connection is in progress.
           socket.connect_nonblock(sockaddr)
-
         rescue IO::WaitWritable
-          # IO.select will block until the socket is writable or the timeout
-          # is exceeded - whichever comes first.
           if IO.select(nil, [socket], nil, timeout)
             begin
-              # Verify there is now a good connection
               socket.connect_nonblock(sockaddr)
             rescue Errno::EISCONN
-              # Good news everybody, the socket is connected!
             rescue
-              # An unexpected exception was raised - the connection is no good.
               socket.close
               raise
             end
           else
-            # IO.select returns nil when the socket is not ready before timeout
-            # seconds have elapsed
             socket.close
             raise TimeoutException
           end
